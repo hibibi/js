75ae51b6baf39e2aeed195ae51208cd1bf5baa3b
hibibi <shinsaku.w@gmail.com>
2012-07-02 15:28:26 +0900

    update

--- mootools/commands/Commands.js
+++ mootools/commands/Commands.js
@@ -181,14 +181,6 @@ var Trace = new Class({
 	},
 	execute: function() {
 		console.log(this._params);
-		/*
-		if (!('console' in window)) {
-			window.console = {};
-			window.console.log = function(this._params){
-				return this._params;
-			};
-		}
-		//*/
 		this.dispatchComplete();
 	}
 });
